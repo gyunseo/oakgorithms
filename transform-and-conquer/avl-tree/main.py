e91a4c371c700ec464a6752edf98f5127886d74b
Gyunseo Lee <rbstj0311@naver.com>
2023-12-10 02:09:41 +0900

    avl tree

--- transform-and-conquer/avl-tree/main.py
+++ transform-and-conquer/avl-tree/main.py
@@ -1,10 +1,3 @@
-"""
-Implementation of an auto-balanced binary tree!
-For doctests run following command:
-python3 -m doctest -v avl_tree.py
-For testing run:
-python avl_tree.py
-"""
 from __future__ import annotations
 
 import math
